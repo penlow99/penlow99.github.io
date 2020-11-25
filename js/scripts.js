14a33de583b6384f5c16d95325da2b8064d94950
penlow99 <penlow99@yahoo.com>
2020-11-25 06:26:38 -0600

    touch added

--- js/scripts.js
+++ js/scripts.js
@@ -77,3 +77,4 @@ function randomNum(min, max) {
 //window.onload = setTimeout(typeWriter, 500);
 //window.onload = setTimeout(fadeFunction, 12000);
 document.addEventListener("keyup", typeWriter);
+document.addEventListener("touchend", typeWriter);
